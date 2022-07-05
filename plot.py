41c1fdb89631e0fe9ca23c9ff42b8f905e1c85c8
Aran Haber <ah2204@cam.ac.uk>
2022-07-05 17:23:47 +0100

    sort data into groups, plot iterations

---
+++ plot.py
@@ -0,0 +1,66 @@
+from data import *
+from distance import *
+import matplotlib.pyplot as plt
+
+# get (lon,lat) coords of addresses in database.xlsx
+# sourced from google sheets extension Geocode by Awesome Table
+datalist = build_data_list()
+coords = [item.coordinates for item in datalist]
+deliverer_coords = [item.coordinates for item in datalist if item.deliverer]
+
+k = len(deliverer_coords) # number of groups to form
+means = deliverer_coords
+
+def plot(coords_list, means_list):
+    """Plot all coordinates in coloured groups, with corresponding means also plotted"""
+    # colours for each group
+    colours = ['c','m','y','g','r','slategrey','orangered'] 
+    cluster_colours = ['b','pink','yellow','darkgreen','firebrick','silver','coral']
+    
+    # set up pyplot figure
+    plt.figure()
+
+    # plot means
+    for item in means_list:
+        plt.plot(item[0], item[1], marker = 'o', markersize=5,\
+        markeredgecolor = 'k', markerfacecolor=colours[means_list.index(item)])
+    
+    # plot coordinates, marking those which are deliverers differently
+    grouped_coords = group_by_means(coords_list, means_list)
+    for i in range(len(grouped_coords)):
+        for item in grouped_coords[i]:
+            if item in deliverer_coords:
+                plt.plot(item[0], item[1], marker='o', markersize=20,\
+                markeredgecolor='k', markerfacecolor=colours[i])
+            else:
+                plt.plot(item[0], item[1], marker='o', markersize=10,\
+                markeredgecolor='none', markerfacecolor=cluster_colours[i])
+    
+    plt.grid()
+    plt.axis('square')
+    plt.show()
+
+def iterate(n, start_means):
+    """Iterate  to a depth of n, plotting each time
+    Break the loop if there is no change in groupings"""
+    grouped = group_by_means(coords, start_means)
+    if n == 0: # finished iterating
+        print("Iteration finished to full depth")
+        return grouped
+    else:
+        new_means = []
+        for i in range(k):
+            new_means.append(centroid(grouped[i]))
+        plot(coords, new_means)
+        plt.show()
+        if grouped == group_by_means(coords, new_means): # previous grouping same as next grouping
+            print("Iteration finished converging, no new changes")
+            return grouped
+        else:
+            iterate(n-1, new_means)
+
+def run():
+    plot(coords, means)
+    iterate(20, means)
+
+run()
