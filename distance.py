546834edf034771cea7cbf20191a58ab837dcacf
Aran Haber <ah2204@cam.ac.uk>
2022-07-05 17:01:20 +0100

    add new centroid function

--- distance.py
+++ distance.py
@@ -37,3 +37,40 @@ def centroid(arr):
     length = len(arr)
     sum_x, sum_y = np.sum(arr, axis=0)
     return sum_x/length, sum_y/length
+
+"""Note that the above centroid() function calculates the centroid of 
+Cartesian coordinates ie. those on a flat plane. Therefore it is not necessarily accurate
+for (lat,lon) coordinates, but can still be used as an approximation - this will be better when
+the coordinates are close together, which they are in the intended use case.
+The below function was then created to get a more realistic estimate of centroids.
+On testing for the sort of data this application is designed for, the two functions are
+equivalent up to 5 decimal places, which corresponds to ~1m of error - completely acceptable for
+this use case"""
+
+def lat_lon_centroid(arr):
+    """Centroid of an array of (lat,lon) coordinates
+    assuming spherical geometry of earth"""
+    # convert lat/lon to Cartesian coordinates for each location.
+    X, Y, Z = 0, 0, 0
+    for coord in arr:
+        # convert to radians
+        lat = coord[0]*np.pi/180
+        lon = coord[1]*np.pi/180
+        X += np.cos(lat) * np.cos(lon)
+        Y += np.cos(lat) * np.sin(lon)
+        Z += np.sin(lat)
+    
+    # Compute average x, y and z coordinates.
+    n = len(arr)
+    X /= n
+    Y /= n
+    Z /= n
+
+    # Convert average x, y, z coordinate back to latitude and longitude.
+    # then convert back to degrees
+    Lon = np.arctan2(Y, X) * 180/np.pi
+    Hyp = np.sqrt(X*X + Y*Y)
+    Lat = np.arctan2(Z, Hyp) * 180/np.pi
+
+    return Lat, Lon
+
